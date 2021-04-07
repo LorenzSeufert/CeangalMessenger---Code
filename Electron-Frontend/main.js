3ba2edf4fc4d4a95be46f9411b41b53463ab9877
Lorenz Seufert <lorenzseufert@gmail.com>
2021-04-07 22:54:01 +0200

    removed react not necessary for the project...
    Took 11 minutes

--- Electron-Frontend/main.js
+++ Electron-Frontend/main.js
@@ -1,21 +1,30 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron')
-const shell = require('electron').shell
-
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'src/preload.js')
+    }
+  })
 
+  win.loadFile('src/index.html')
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
   }
 })
