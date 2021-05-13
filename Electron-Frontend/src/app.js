33b3472febee3072426d62ac63b672b7387a7d11
Lorenz Seufert <lorenzseufert@gmail.com>
2021-05-13 20:33:10 +0200

    added dependency for packaging and icon.ico
    Took 55 minutes

--- Electron-Frontend/src/app.js
+++ Electron-Frontend/src/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("src/public"));
+app.use(express.static(__dirname + "/public"));
 app.set('views', path.join(__dirname, '../src/views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
