5c922e2151964c84611eeb0fa4b1999c9a28a905
David Bullinger <david.bullinger@gmx.de>
2021-06-05 11:48:01 +0200

    added documentation to the functions of "UserController"
    Took 16 minutes

--- Kotlin-Backend/src/main/kotlin/com/dhbw/ceangal/usermodel/UserController.kt
+++ Kotlin-Backend/src/main/kotlin/com/dhbw/ceangal/usermodel/UserController.kt
@@ -11,19 +11,36 @@ import org.springframework.web.bind.annotation.*
 class UserController {
     @Autowired
     private lateinit var userService: UserService
-
+    /**
+     * This function represents the REST-entry for creating a user
+     *
+     * @param userProfile the user to be created
+     * @return responseMessage the message and status which is send back as a response
+     */
     @PostMapping(value = ["/user/createUser"])
     fun createUser(@RequestBody userProfile:UserProfile): ResponseEntity<String> {
         userService.createUser(userProfile)
         return ResponseEntity("User creating was succesful.", HttpStatus.OK)
     }
-
+    /**
+     * This function represents the REST-entry for editing a user
+     *
+     * @param userProfile data which contains the requested fields to be changed
+     * @param id the id of the user whose data should be changed
+     * @return responseMessage the full changed user and status which is send back as a response
+     */
     @PutMapping(value = ["/user/editUser/{id}"])
     fun editUser(@RequestBody userProfile:UserProfile, @PathVariable id: Long): ResponseEntity<UserProfile> {
         val user = userService.editUser(userProfile, id)
         return ResponseEntity(user, HttpStatus.OK)
     }
 
+    /**
+     * This function represents the REST-entry for deleting a user
+     *
+     * @param id the id of the user who should be deleted
+     * @return responseMessage the message and status which is send back as a response
+     */
     @DeleteMapping(value = ["/user/deleteUser/{id}"])
     fun deleteUser(@PathVariable id: Long): ResponseEntity<String> {
         userService.deleteUser(id)
