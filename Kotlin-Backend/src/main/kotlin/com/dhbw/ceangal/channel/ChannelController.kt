37988489b658ceda054575e6d27fe541d87b08c0
David Bullinger <david.bullinger@gmx.de>
2021-06-14 15:36:18 +0200

    deleted unused imports
    Took 31 minutes

--- Kotlin-Backend/src/main/kotlin/com/dhbw/ceangal/channel/ChannelController.kt
+++ Kotlin-Backend/src/main/kotlin/com/dhbw/ceangal/channel/ChannelController.kt
@@ -1,7 +1,5 @@
 package com.dhbw.ceangal.channel
 
-import com.dhbw.ceangal.login.LoginModel
-import com.google.gson.Gson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
