e1ed6f88f45a4c503171f624ce39823f7a6d4398
David Bullinger <david.bullinger@gmx.de>
2021-06-17 19:10:53 +0200

    CMR-13 Writing Integration tests for UserController (creating deleting and editing a user)
    Took 17 minutes

--- Kotlin-Backend/src/test/kotlin/com/dhbw/ceangal/user/editUserRestTest.kt
+++ Kotlin-Backend/src/test/kotlin/com/dhbw/ceangal/user/editUserRestTest.kt
@@ -1,29 +1,29 @@
 package com.dhbw.ceangal.user
 
+import com.dhbw.ceangal.usermodel.UserController
 import com.dhbw.ceangal.usermodel.UserProfile
 import com.dhbw.ceangal.usermodel.UserService
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.assertEquals
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class editUserTest {
+class editUserRestTest {
+    @Autowired
+    lateinit var userController: UserController
     @Autowired
     lateinit var userService: UserService
-
     @Test
     fun editUserTest() {
         var userProfile = UserProfile(1, "User1", "password", "abc@gmx.de", "01.01.2000", "description")
-        userService.createUser(userProfile)
+        userController.createUser(userProfile)
         val sessionId = userService.login(userProfile)
         val userProfileChange = UserProfile(0, "NewName", "", "", "", "")
-        val userProfileChanged = userService.editUser(userProfileChange, sessionId)
-        val expectedUserProfile = UserProfile(1, "NewName", "password", "abc@gmx.de", "01.01.2000", "description")
-        assertEquals(expectedUserProfile.toString(), userProfileChanged.toString())
+        assertEquals(HttpStatus.OK, userController.editUser(userProfileChange, sessionId).statusCode)
     }
-
 }
