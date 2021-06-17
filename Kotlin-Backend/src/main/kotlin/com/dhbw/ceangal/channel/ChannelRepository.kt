cd7365c88be0d10efd2791a478775b01a54f35fb
TiKsers <fabian.dittebrand@web.de>
2021-06-17 13:43:47 +0200

    removed channel
    Took 5 minutes

--- Kotlin-Backend/src/main/kotlin/com/dhbw/ceangal/channel/ChannelRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.dhbw.ceangal.channel
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ChannelRepository : JpaRepository <Channel, Long>
